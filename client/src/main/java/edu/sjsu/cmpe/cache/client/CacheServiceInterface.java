bf25cb6545f58825785ce2356528e249815d3aa1
krutipandya <kruti.pandya90@gmail.com>
2015-05-16 19:34:22 -0700

    lab-3

---
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/CacheServiceInterface.java
@@ -0,0 +1,7 @@
+package edu.sjsu.cmpe.cache.client;
+
+public interface CacheServiceInterface {
+    public String get(long key);
+    public void put(long key, String value);
+    public String getCacheServerUrl();
+}
